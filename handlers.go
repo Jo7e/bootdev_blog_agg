72040087395d5c0676ed0bc2c6f6e2f5be783797
Jo7e <work.jorge.salazar@gmail.com>
2024-06-03 02:03:31 -0400

    feat: chapter 4

--- handlers.go
+++ handlers.go
@@ -1,6 +1,13 @@
 package main
 
-import "net/http"
+import (
+	"bootdev_blog_agg/internal/database"
+	"encoding/json"
+	"net/http"
+	"time"
+
+	"github.com/google/uuid"
+)
 
 func HealthzHandler(w http.ResponseWriter, r *http.Request) {
 	respondWithJSON(w, http.StatusOK, map[string]string{"status": "ok"})
@@ -8,4 +15,36 @@ func HealthzHandler(w http.ResponseWriter, r *http.Request) {
 
 func ErrHandler(w http.ResponseWriter, r *http.Request) {
 	respondWithError(w, http.StatusInternalServerError, "Internal Server Error")
+}
+
+type CreateUserRequest struct {
+	Name string `json:"name"`
+}
+
+func (a *apiConfig) CreateUserHandler(w http.ResponseWriter, r *http.Request) {
+	ctx := r.Context()
+
+	userRequest := CreateUserRequest{}
+	err := json.NewDecoder(r.Body).Decode(&userRequest)
+	if err != nil {
+		respondWithError(w, http.StatusBadRequest, "Invalid request payload")
+		return
+	}
+
+	now := time.Now()
+
+	user := database.CreateUserParams{
+		ID:        uuid.New(),
+		CreatedAt: now,
+		UpdatedAt: now,
+		Name:      userRequest.Name,
+	}
+
+	userResponse, err := a.DB.CreateUser(ctx, user)
+	if err != nil {
+		respondWithError(w, http.StatusInternalServerError, err.Error())
+		return
+	}
+
+	respondWithJSON(w, http.StatusCreated, userResponse)
 }
--- main.go
+++ main.go
@@ -1,19 +1,39 @@
 package main
 
 import (
+	"bootdev_blog_agg/internal/database"
+	"database/sql"
+	"log"
 	"net/http"
 	"os"
 
 	"github.com/joho/godotenv"
+	_ "github.com/lib/pq"
 )
+
+type apiConfig struct {
+	DB *database.Queries
+}
 
 func main() {
 	err := godotenv.Load()
 	if err != nil {
-		panic(err)
+		log.Fatal(err)
 	}
 
-	add := os.Getenv("HOST") + os.Getenv("PORT")
+	dbURL := os.Getenv("DB_URL")
+	db, err := sql.Open("postgres", dbURL)
+
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	dbQueries := database.New(db)
+	apiConfig := &apiConfig{
+		DB: dbQueries,
+	}
+
+	add := os.Getenv("HOST") + ":" + os.Getenv("PORT")
 	m := http.NewServeMux()
 
 	s := http.Server{
@@ -21,8 +41,9 @@ func main() {
 		Handler: m,
 	}
 
+	m.HandleFunc("GET /v1/users", apiConfig.CreateUserHandler)
 	m.HandleFunc("GET /v1/healthz", HealthzHandler)
 	m.HandleFunc("GET /v1/err", ErrHandler)
 
-	s.ListenAndServe()
+	log.Fatal(s.ListenAndServe())
 }
