a60a440c45e391a3fe8a763d3d89e64cf4701d44
Jore Salazar <work.jorge.salazar@gmail.com>
2024-06-25 00:55:58 -0400

    feat: added mappers

--- pkg/handlers/getfeeds.go
+++ pkg/handlers/getfeeds.go
@@ -3,43 +3,19 @@ package handlers
 import (
 	"bootdev_blog_agg/internal"
 	"net/http"
-	"time"
-
-	"github.com/google/uuid"
 )
-
-type GetFeedsResponse struct {
-	ID        uuid.UUID `json:"id"`
-	Name      string    `json:"name"`
-	Url       string    `json:"url"`
-	UserID    uuid.UUID `json:"user_id"`
-	CreatedAt time.Time `json:"created_at"`
-	UpdatedAt time.Time `json:"updated_at"`
-}
 
 func GetFeedsHandler(a *ApiConfig) http.HandlerFunc {
 	return func(w http.ResponseWriter, r *http.Request) {
 		ctx := r.Context()
 
-		feeds, err := a.DB.GetFeeds(ctx)
+		databaseFeeds, err := a.DB.GetFeeds(ctx)
 		if err != nil {
 			internal.RespondWithError(w, http.StatusInternalServerError, err.Error())
 			return
 		}
-		feedsResponse := make([]GetFeedsResponse, 0, len(feeds))
+		feeds := internal.DataBaseFeedsToFeeds(databaseFeeds)
 
-		for _, feed := range feeds {
-			feedResponse := GetFeedsResponse{
-				ID:        feed.ID,
-				Name:      feed.Name,
-				Url:       feed.Url,
-				UserID:    feed.UserID,
-				CreatedAt: feed.CreatedAt,
-				UpdatedAt: feed.UpdatedAt,
-			}
-			feedsResponse = append(feedsResponse, feedResponse)
-		}
-
-		internal.RespondWithJSON(w, http.StatusCreated, feedsResponse)
+		internal.RespondWithJSON(w, http.StatusCreated, feeds)
 	}
 }
